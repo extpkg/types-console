b5a200e5e612246de38d2369f09f0d4f1eb27378
Casper <casper@ext.store>
2023-08-24 07:10:28 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,1 @@
+# Types for EXT console module
---
+++ index.d.ts
@@ -0,0 +1,37 @@
+/** EXT console module. */
+declare namespace console {
+
+  /**
+   * Log to console.
+   * @param message Message or object to write to console
+   * @param optionalParams Additional messages or objects to write
+   */
+  export function log(message?: any, ...optionalParams: any[]): void
+
+  /** Alias for {@link log}. */
+  export function info(message?: any, ...optionalParams: any[]): void
+
+  /** Alias for {@link log}. */
+  export function debug(message?: any, ...optionalParams: any[]): void
+
+  /**
+   * Log an error to console.
+   * @param message Message or object to write to console
+   * @param optionalParams Additional messages or objects to write
+   */
+  export function error(message?: any, ...optionalParams: any[]): void
+  
+  /** Alias for {@link error}. */
+  export function warn(message?: any, ...optionalParams: any[]): void
+
+  /**
+   * Log to console with a stack trace.
+   * @param message Message or object to write to console
+   * @param optionalParams Additional messages or objects to write
+   */
+  export function trace(message?: any, ...optionalParams: any[]): void
+
+  /** Clear console if possible. */
+  export function clear(): void
+
+}
